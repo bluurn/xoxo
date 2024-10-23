1388e9e17bbde2287cd865f4920049e1387c6f11
Vladimir Suvorov <vladimir.suvorov@xempus.com>
2024-10-23 17:16:08 +0200

    Render X and O

--- xoxo.c
+++ xoxo.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "raylib.h"
 #include "render.h"
+#include "raymath.h"
 
 #define WINDOW_TITLE "Tic Tac Toe"
 #define INIT_SCREEN_WIDTH 1024
@@ -14,17 +15,66 @@ void ReadEvents()
 }
 
 
+void RenderX(int x, int y) {
+  float w = (float)GetScreenWidth() / N;
+  float h = (float)GetScreenHeight() / N;
+
+  // TODO make lines orthogonal
+
+  Vector2 tl = { w / 3, h / 3 };
+  Vector2 br = { w - w / 3, h - h / 3 };
+  Vector2 bl = { w / 3, h - h / 3 };
+  Vector2 tr = { w - w / 3, h / 3};
+  Vector2 delta = { w * x, h * y};
+
+  DrawLineEx(Vector2Add(tl, delta), Vector2Add(br, delta), 10, RAYWHITE);
+  DrawLineEx(Vector2Add(bl, delta), Vector2Add(tr, delta), 10, RAYWHITE);
+}
+
+void RenderO(int x, int y)
+{
+  float w = (float)GetScreenWidth() / N;
+  float h = (float)GetScreenHeight() / N;
+  float radius = fmin(w, h) / 4; 
+  Vector2 center = { w / 2, h / 2 };
+  Vector2 delta = { w * x, h * y};
+
+  DrawRing(Vector2Add(center, delta), radius - 10, radius, 0.0f, 360.0f, 0, MAROON);
+}
+
+void RenderCellValue(Cell value, int x, int y)
+{
+  switch(value) {
+
+  case E:
+    break;
+  case X:
+    RenderX(x, y);
+    break;
+  case O:
+    RenderO(x, y);
+    break;
+  }
+}
+
+
 void RenderGame(const Game * game) 
 {
+  for(int y = 0; y < N; ++y) {
+    for(int x = 0; x < N; ++x) {
+      Cell value = getCell(game, x, y);
+      RenderCellValue(value, x, y);
+    }
+  }
 }
 
 int main(void)
 {
   Game game = {
     .board = { 
-      E, E, E,
-      E, E, E,
-      E, E, E,
+      X, X, E,
+      O, X, O,
+      E, O, E,
     },
     .state = PLAYER_X_TURN
   };
